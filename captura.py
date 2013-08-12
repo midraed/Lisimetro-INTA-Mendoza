0c08cd50439f49c944811d5fc5c2487e21b1c547
Guillermo Federico Olmedo <guillermo.olmedo@gmail.com>
2013-08-12 16:20:15 -0300

    cambiamos el protocolo de comunicacion

--- captura.py
+++ captura.py
@@ -4,42 +4,30 @@ import MySQLdb
 import time
 import re
 
-lisimetro = serial.Serial( '/dev/ttyS0', 9600, timeout=5)
-BD = MySQLdb.connect( host='localhost', db='LISIMETRO', user='guillermo', passwd='****' )
+lisimetro = serial.Serial( '/dev/ttyS0', 9600, timeout=None)
+BD = MySQLdb.connect( host='localhost', db='LISIMETRO', user='guillermo', passwd='******' )
 curs = BD.cursor()
-s = 'A'
-ultimo = 'A'
+s = ['A', 'B']
+ultimo = ['A', 'B']
 
 while True:
   s = lisimetro.readline().strip("\r\n")
   if s != ultimo:
-   if s[0] == 'S': ## Aca va para sensores:
             ultimo = s
-            s = re.findall(r'\d+', s)
-            #s[0] = (5.0 * float(s[0]) * 100.0)/1024.0 - 273.15 Por ej. para convertir de mV a Temp 
-            #s[1] = (5.0 * float(s[1]) * 100.0)/1024.0 - 273.15
-            #s[2] = (5.0 * float(s[2]) * 100.0)/1024.0 - 273.15
+            LIS = re.findall('LIS (.*?)KG', s)
+            MOV = re.findall('KG(.*?) \|', s)
+            R0 = re.findall('ESCLAVO_0 (.*?)\|',s)
+            R0 = R0[0].split(' ')
+            ESTMET = re.findall('ESTMET (.*?)\]',s)
             curs.execute(
-                 'insert into Sensores'
-                 '(Fecha, Temp1_50, Temp2_100, Temp3_150, SENS4, SENS5, SENS6) '
+                 'insert into prueba2'
+                 '(Fecha, Peso, Inestable, TEMP50, TEMP100, TEMP150, TENS50, ESTMET) '
                  'VALUES '
-                 '(%s, %s, %s, %s, %s, %s, %s)',
-                 (time.strftime("%Y-%m-%d %H:%M:%S"), s[0], s[1], s[2], s[3], s[4], s[5]))
+                 '(%s, %s, %s, %s, %s, %s, %s, %s)',
+                 (time.strftime("%Y-%m-%d %H:%M:%S"), float(LIS[0]), MOV[0]=='M', R0[0], R0[2], R0[4], R0[5], ESTMET[0]))
             BD.commit()
-   else: #Aca va pesada
-             ultimo = s
-             signo = re.findall(r'-', s)
-             peso = re.findall(r'\d+', s)
-             estabilidad = re.findall(r'M', s)
-             peso =  int(''.join(signo+peso))
-             curs.execute(
-                 'insert into Pesada'
-                 '(Fecha, Peso, Estable) '
-                 'VALUES '
-                 '(%s, %s, %s)',
-                 (time.strftime("%Y-%m-%d %H:%M:%S"), str(peso), estabilidad=='M'))
-             BD.commit()
   else:
      pass
-  time.sleep(30)
+  time.sleep(100)
+
   
