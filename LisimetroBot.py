9dc9eeaf7a16f03e978a5e7fd518d223206b626f
Guillermo Olmedo <guillermo.olmedo@gmail.com>
2016-11-03 20:10:40 -0300

    the same`

--- LisimetroBot.py
+++ LisimetroBot.py
@@ -8,7 +8,12 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Ohh yeah")
+    bot.reply_to(message, "LISIMETRO EEA MENDOZA")
+
+@bot.message_handler(regexp="tiempo")
+def handle_message(message):
+    bot.reply_to(message, "Hacen 23 grados")
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
