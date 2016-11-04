b07565c62693f1f7652cf9e056a075b2d45c56ad
Guillermo Olmedo <guillermo.olmedo@gmail.com>
2016-11-04 16:05:38 -0300

    Added forecast

--- LisimetroBot.py
+++ LisimetroBot.py
@@ -1,25 +1,25 @@
 import telebot
 import pyowm
 
-f = open('TOKEN.txt')
+f = open('/home/guillermo/Lisimetro-INTA-Mendoza/TOKEN.txt')
 TOKEN = f.read().strip('\n')
 f.close
 
-f = open('OWM.key')
+f = open('/home/guillermo/Lisimetro-INTA-Mendoza/OWM.key')
 OWM = f.read().strip('\n')
 f.close
 
-owm = pyowm.OWM(OWM) 
+owm = pyowm.OWM(OWM, language='es') 
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "LISIMETRO EEA MENDOZA")
+    bot.reply_to(message, "Estacion Lisimetrica\nINTA EEA Mendoza")
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, "Este bot es experimental, por ahora solo interpreta palabras como tiempo, temperatura, lluvia.\n Responde comandos como /start, /help y /status")
+    bot.reply_to(message, "Este bot es experimental, por ahora solo interpreta palabras como tiempo, temperatura, lluvia.\nResponde comandos como /start, /help y /status")
 
 @bot.message_handler(commands=['status'])
 def send_welcome(message):
@@ -33,8 +33,19 @@ def handle_message(message):
     # Weather details
     wind = w.get_wind()                  # {'speed': 4.6, 'deg': 330}
     hum = w.get_humidity()              # 87
+    pres = w.get_pressure() 
     temp = w.get_temperature('celsius')
-    bot.reply_to(message, "Hacen " +  str(temp['temp']) + " grados \n El viento es de " + str(wind['speed']) + " km/h \n La humedad relativa es de " + str(hum) + " %.")
+    bot.reply_to(message, "Hacen " +  str(temp['temp']) + " grados \nEl viento es de " + str(wind['speed']) + " km/h \nLa humedad relativa es de " + str(hum) + " %.\nLa presión es de" + str(pres['press']) + " mb.")
+
+
+@bot.message_handler(regexp="pronostico")
+def handle_message(message):
+    fc = owm.daily_forecast('Vistalba,AR', limit=6)
+    fore = fc.get_forecast()
+    lst = fore.get_weathers()
+    bot.reply_to(message, str(lst))
+
+
 
 @bot.message_handler(regexp="lloviendo")
 def handle_message(message):
@@ -43,6 +54,12 @@ def handle_message(message):
 @bot.message_handler(regexp="temperatura")
 def handle_message(message):
     bot.reply_to(message, "Esta para una birra")
+
+@bot.message_handler(regexp="padre")
+def handle_message(message):
+    bot.reply_to(message, "Luke, I am your father")
+
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
