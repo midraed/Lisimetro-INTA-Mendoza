f6ba1a44f6552bdba7d92ad17729268eab28ef91
Guillermo Olmedo <guillermo.olmedo@gmail.com>
2016-11-07 17:14:40 -0300

    Added emojis to weather status

--- LisimetroBot.py
+++ LisimetroBot.py
@@ -32,6 +32,47 @@ def extract_point_from_raster(point, data_source, band_number=1):
 
     return intval[0] 
 
+def getEmoji(weatherID):
+    if weatherID:
+        if str(weatherID)[0] == '2' or weatherID == 900 or weatherID==901 or weatherID==902 or weatherID==905:
+            return thunderstorm
+        elif str(weatherID)[0] == '3':
+            return drizzle
+        elif str(weatherID)[0] == '5':
+            return rain
+        elif str(weatherID)[0] == '6' or weatherID==903 or weatherID== 906:
+            return snowflake + ' ' + snowman
+        elif str(weatherID)[0] == '7':
+            return atmosphere
+        elif weatherID == 800:
+            return clearSky
+        elif weatherID == 801:
+            return fewClouds
+        elif weatherID==802 or weatherID==803 or weatherID==803:
+            return clouds
+        elif weatherID == 904:
+            return hot
+        else:
+            return defaultEmoji    # Default emoji
+    else:
+        return defaultEmoji # Default emoji
+
+
+
+######### Emojis (from Weather Bot)
+
+thunderstorm = u'\U0001F4A8'    # Code: 200's, 900, 901, 902, 905
+drizzle = u'\U0001F4A7'         # Code: 300's
+rain = u'\U00002614'            # Code: 500's
+snowflake = u'\U00002744'       # Code: 600's snowflake
+snowman = u'\U000026C4'         # Code: 600's snowman, 903, 906
+atmosphere = u'\U0001F301'      # Code: 700's foogy
+clearSky = u'\U00002600'        # Code: 800 clear sky
+fewClouds = u'\U000026C5'       # Code: 801 sun behind clouds
+clouds = u'\U00002601'          # Code: 802-803-804 clouds general
+hot = u'\U0001F525'             # Code: 904
+defaultEmoji = u'\U0001F300' # default emojis
+
 ####### BOT
 
 f = open('/home/guillermo/Lisimetro-INTA-Mendoza/TOKEN.txt')
@@ -97,12 +138,14 @@ def handle_message(message):
     # TODO: Don't ask for a new observation if this one is from the last 10 minutes
     w = observation.get_weather()
     # Weather details
-    w_status = w.get_detailed_status() 
+    w_status = w.get_detailed_status()
     wind = w.get_wind()
     hum = w.get_humidity()
     pres = w.get_pressure()
     temp = w.get_temperature('celsius')
-    bot.reply_to(message, str(w_status)
+    weatherID = w.get_weather_code()   
+    emoji = getEmoji(weatherID)
+    bot.reply_to(message, str(w_status) + " " + emoji + emoji
     + "\nHacen " +  str(temp['temp']) 
     + " grados \nEl viento es de " + str(wind['speed']) 
     + " km/h \nLa humedad relativa es de " + str(hum) 
@@ -149,9 +192,11 @@ def handle_location(message):
        hum = w.get_humidity()
        pres = w.get_pressure()
        temp = w.get_temperature('celsius')
+       weatherID = w.get_weather_code() 
+       emoji = getEmoji(weatherID)
        bot.reply_to(message, "Solo puedo dar datos de evapotranspiracion cerca de la estación, "
        + "pero puedo buscar el estado del tiempo en [owm](http://openweathermap.org/) para esa localización: "
-       + "\n" + str(w_status)
+       + "\n" + str(w_status) + " "  + emoji + emoji
        + "\nHacen " +  str(temp['temp']) + " grados \nEl viento es de " + str(wind['speed'])
        + " km/h \nLa humedad relativa es de " + str(hum) + "%.\nLa presión es de " + str(pres['press']) + " mb.",
        parse_mode = "Markdown")
